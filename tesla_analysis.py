8f77fa4d9ff1ee45eafa428a3fad8f26f196c28f
ArjavRD <33216545+ArjavRD@users.noreply.github.com>
2025-03-04 17:39:24 -0800

    Update tesla_analysis.py

--- tesla_analysis.py
+++ tesla_analysis.py
@@ -4,7 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 TSLA_2=pd.read_csv("Tesla 15th oct 2024- 5th nov 2024.csv")
-TSLA_2.head()
+print(TSLA_2.head())
+TSLA_3=pd.read_csv("TSLA_period3.csv")
+TSLA_2.columns = TSLA_2.columns.str.strip()
 sma2=TSLA_2['Close'].mean()
 print(f"The SMA for period 2 is: {sma2}")
 
@@ -13,4 +15,12 @@ ema2 = TSLA_2['Close'].ewm(span=20, adjust=False).mean()
 
 # Print the last few EMA values
 print(f"The last few EMA values for period 2 are: \n{ema2.head()}")
+TSLA_3.columns = TSLA_3.columns.str.strip()
+sma3=TSLA_3['Close'].mean()
+print(f"The SMA for period 3 is: {sma3}")
 
+# Calculate the Exponential Moving Average (EMA)
+ema3 = TSLA_3['Close'].ewm(span=20, adjust=False).mean()
+
+# Print the last few EMA values
+print(f"The last few EMA values for period 3 are: \n{ema3.head()}")
